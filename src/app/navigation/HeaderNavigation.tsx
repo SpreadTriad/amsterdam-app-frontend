22bdd02299bb4913e4016189b63b9d835ba7579e
Frank Filius <f.filius@amsterdam.nl>
2022-03-21 14:45:35 +0100

    Render badge in header

--- src/app/navigation/HeaderNavigation.tsx
+++ src/app/navigation/HeaderNavigation.tsx
@@ -1,19 +1,13 @@
 import {useNavigation} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
-import React, {ReactNode} from 'react'
+import React from 'react'
 import {Insets, StyleSheet, TouchableOpacity} from 'react-native'
 import {BellInactive, Settings} from '../../assets/icons'
+import {NotificationsCounter} from '../../components/features/notifications'
 import {Row} from '../../components/ui/layout'
 import {color, size} from '../../tokens'
 import {routes} from './routes'
 import {StackParams, TabParams} from './types'
-
-type MenuItem = {
-  icon: ReactNode
-  label: string
-  name: string
-  route: keyof StackParams
-}
 
 const hitSlop: Insets = {
   top: size.spacing.sm,
@@ -26,43 +20,41 @@ const iconProps = {
   fill: color.font.regular,
 }
 
-const menu: MenuItem[] = [
-  {
-    icon: <BellInactive {...iconProps} />,
-    label: 'Berichten',
-    name: 'notifications',
-    route: routes.notificationOverview.name,
-  },
-  {
-    icon: <Settings {...iconProps} />,
-    label: 'Instellingen',
-    name: 'settings',
-    route: routes.settings.name,
-  },
-]
-
 export const HeaderNavigation = () => {
   const navigation =
     useNavigation<StackNavigationProp<StackParams & TabParams, 'Home'>>()
 
   return (
     <Row gutter="md">
-      {menu.map(({icon, label, name, route}) => (
-        <TouchableOpacity
-          accessibilityLabel={label}
-          accessibilityRole="button"
-          hitSlop={hitSlop}
-          key={name}
-          onPress={() => navigation.navigate(route)}
-          style={styles.icon}>
-          {icon}
-        </TouchableOpacity>
-      ))}
+      <TouchableOpacity
+        accessibilityLabel="Berichten"
+        accessibilityRole="button"
+        hitSlop={hitSlop}
+        key="notifications"
+        onPress={() => navigation.navigate(routes.notificationOverview.name)}
+        style={styles.icon}>
+        <BellInactive {...iconProps} />
+        <NotificationsCounter style={styles.badge} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        accessibilityLabel="Instellingen"
+        accessibilityRole="button"
+        hitSlop={hitSlop}
+        key="settings"
+        onPress={() => navigation.navigate(routes.settings.name)}
+        style={styles.icon}>
+        <Settings {...iconProps} />
+      </TouchableOpacity>
     </Row>
   )
 }
 
 const styles = StyleSheet.create({
+  badge: {
+    position: 'absolute',
+    right: -12,
+    top: -5,
+  },
   icon: {
     width: 24,
     aspectRatio: 1,
